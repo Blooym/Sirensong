c599b0b9e1fcbba4cd5283abdaff94e113b732d3
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2023-01-20 12:22:11 +0000

    feat: move colour for role code

--- Sirensong/Game/Enums/ClassJobRole.cs
+++ Sirensong/Game/Enums/ClassJobRole.cs
@@ -1,6 +1,6 @@
-using Dalamud.Interface.Colors;
 using FFXIVClientStructs.FFXIV.Common.Math;
 using Lumina.Excel.GeneratedSheets;
+using Sirensong.UserInterface;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 namespace Sirensong.Game.Enums
@@ -8,7 +8,7 @@ namespace Sirensong.Game.Enums
     /// <summary>
     ///     A mapping of <see cref="ClassJob"/> role IDs to their names.
     /// </summary>
-    public enum ClassJobRole
+    public enum ClassJobRole : uint
     {
         Misc = 0,
         Tank = 1,
@@ -23,13 +23,6 @@ namespace Sirensong.Game.Enums
     /// </summary>
     public static class ClassJobRoleExtensions
     {
-        public static Vector4 GetColourForRole(byte classID) => classID switch
-        {
-            1 => ImGuiColors.TankBlue,
-            2 => ImGuiColors.DPSRed,
-            3 => ImGuiColors.DPSRed,
-            4 => ImGuiColors.HealerGreen,
-            _ => ImGuiColors.DalamudGrey
-        };
+        public static Vector4 GetColourForRole(this ClassJobRole roleID) => SiUI.GetColourForRole((uint)roleID);
     }
 }
