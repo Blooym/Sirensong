217dbd51c5ac353a9d317e43d7acb6cb6fb16074
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2023-01-27 21:55:30 +0000

    feat: dont wait until edit

--- Sirensong/UserInterface/Input.cs
+++ Sirensong/UserInterface/Input.cs
@@ -16,11 +16,11 @@ namespace Sirensong.UserInterface
         /// <param name="maxLength">The max length of the text input field </param>
         /// <param name="debounced">Whether or not the input should be debounced.</param>
         /// <param name="flags">The <see cref="ImGuiInputTextFlags"/> to use.</param>
-        /// <returns>True when either the input is typed into or when it is deactivated after being edited if debounced.</returns>
+        /// <returns>True when either the input is typed into or when it is deactivated if debounced.</returns>
         public static bool InputText(string label, ref string value, uint maxLength, bool debounced = false, ImGuiInputTextFlags flags = default)
         {
             var inputBool = ImGui.InputText(label, ref value, maxLength, flags);
-            return debounced ? ImGui.IsItemDeactivatedAfterEdit() : inputBool;
+            return debounced ? ImGui.IsItemDeactivated() : inputBool;
         }
 
         /// <summary>
@@ -32,11 +32,11 @@ namespace Sirensong.UserInterface
         /// <param name="maxLength">The max length of the text input field</param>
         /// <param name="debounced">Whether or not the input should be debounced.</param>
         /// <param name="flags">The <see cref="ImGuiInputTextFlags"/> to use.</param>
-        /// <returns>True when either the input is typed into or when it is deactivated after being edited if debounced.</returns>
+        /// <returns>True when either the input is typed into or when it is deactivated if debounced.</returns>
         public static bool InputTextHint(string label, string hint, ref string value, uint maxLength, bool debounced = false, ImGuiInputTextFlags flags = default)
         {
             var inputBool = ImGui.InputTextWithHint(label, hint, ref value, maxLength, flags);
-            return debounced ? ImGui.IsItemDeactivatedAfterEdit() : inputBool;
+            return debounced ? ImGui.IsItemDeactivated() : inputBool;
         }
 
         /// <summary>
@@ -48,11 +48,11 @@ namespace Sirensong.UserInterface
         /// <param name="size">The size of the text input field.</param>
         /// <param name="debounced">Whether or not the input should be debounced.</param>
         /// <param name="flags">The <see cref="ImGuiInputTextFlags"/> to use.</param>
-        /// <returns>True when either the input is typed into or when it is deactivated after being edited if debounced.</returns>
+        /// <returns>True when either the input is typed into or when it is deactivated if debounced.</returns>
         public static bool InputTextMultiline(string label, ref string value, uint maxLength, Vector2 size, bool debounced = false, ImGuiInputTextFlags flags = default)
         {
             var inputBool = ImGui.InputTextMultiline(label, ref value, maxLength, size, flags);
-            return debounced ? ImGui.IsItemDeactivatedAfterEdit() : inputBool;
+            return debounced ? ImGui.IsItemDeactivated() : inputBool;
         }
 
         /// <summary>
@@ -64,11 +64,11 @@ namespace Sirensong.UserInterface
         /// <param name="stepFast">The step to increment/decrement the value by when going fast.</param>
         /// <param name="debounced">Whether or not the input should be debounced</param>
         /// <param name="flags">The <see cref="ImGuiInputTextFlags"/> to use.</param>
-        /// <returns>True when either the input is typed into or when it is deactivated after being edited if debounced.</returns>
+        /// <returns>True when either the input is typed into or when it is deactivated if debounced.</returns>
         public static bool InputInt(string label, ref int value, int step, int stepFast, bool debounced = false, ImGuiInputTextFlags flags = default)
         {
             var inputBool = ImGui.InputInt(label, ref value, step, stepFast, flags);
-            return debounced ? ImGui.IsItemDeactivatedAfterEdit() : inputBool;
+            return debounced ? ImGui.IsItemDeactivated() : inputBool;
         }
 
         /// <summary>
@@ -81,11 +81,11 @@ namespace Sirensong.UserInterface
         /// <param name="format">The format to display the value in.</param>
         /// <param name="debounced">Whether or not the input should be debounced</param>
         /// <param name="flags">The <see cref="ImGuiInputTextFlags"/> to use.</param>
-        /// <returns>True when either the input is typed into or when it is deactivated after being edited if debounced.</returns>
+        /// <returns>True when either the input is typed into or when it is deactivated if debounced.</returns>
         public static bool InputFloat(string label, ref float value, float step, float stepFast, string format, bool debounced = false, ImGuiInputTextFlags flags = default)
         {
             var inputBool = ImGui.InputFloat(label, ref value, step, stepFast, format, flags);
-            return debounced ? ImGui.IsItemDeactivatedAfterEdit() : inputBool;
+            return debounced ? ImGui.IsItemDeactivated() : inputBool;
         }
     }
 }
