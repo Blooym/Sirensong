84e71ebb2dd5a83b89b6f6a9bbb829cac38421b5
Blooym <19539165+Blooym@users.noreply.github.com>
2023-10-03 18:52:42 +0000

    more v9 compat

--- Sirensong/SharedServices.cs
+++ Sirensong/SharedServices.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
 using Dalamud.Game.ClientState.Objects;
-using Dalamud.Game.Gui;
 using Dalamud.Interface;
 using Dalamud.IoC;
 using Dalamud.Plugin;
@@ -24,13 +23,14 @@ namespace Sirensong
         [PluginService] internal static IClientState ClientState { get; private set; } = null!;
         [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
         [PluginService] internal static IToastGui ToastGui { get; private set; } = null!;
-        [PluginService] internal static ChatGui ChatGui { get; private set; } = null!;
+        [PluginService] internal static IChatGui ChatGui { get; private set; } = null!;
         [PluginService] internal static ITargetManager TargetManager { get; private set; } = null!;
         [PluginService] internal static IObjectTable ObjectTable { get; private set; } = null!;
         [PluginService] internal static ICondition Condition { get; private set; } = null!;
         [PluginService] internal static IFramework Framework { get; private set; } = null!;
         [PluginService] internal static DalamudPluginInterface PluginInterface { get; private set; } = null!;
         [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
+        [PluginService] internal static IPluginLog PluginLog { get; private set; } = null!;
 
         // Dalamud service shortcuts
         internal static UiBuilder UiBuilder => PluginInterface.UiBuilder;
